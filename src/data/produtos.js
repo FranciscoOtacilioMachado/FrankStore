744d0df1b89330daf50d231c926c5e8195ff7c9d
franciscootacilio <85141115+fransciscootacilio@users.noreply.github.com>
2026-09-09 22:48:56 -0300

    adiciona array de produtos fixo

---
+++ src/data/produtos.js
@@ -0,0 +1,23 @@
+export const produtos = [
+    {
+        id: 1,
+        nome: "Camisa de botão",
+        precoUnitario: 79.9,
+        quantidade: 2,
+        imagem: CamisaBotao,
+    },
+    {
+        id: 2,
+        nome: "Boné Nova",
+        precoUnitario: 49.8,
+        quantidade: 3,
+        imagem: BoneImg,
+    },
+    {
+        id: 3,
+        nome: "Bermuda",
+        precoUnitario: 45.5,
+        quantidade: 1,
+        imagem: BermudaImg,
+    },
+]
