22df273ca400c807c0c756c95d4edb3e994156cd
franciscootacilio <85141115+fransciscootacilio@users.noreply.github.com>
2026-09-10 22:05:44 -0300

    adiciona schema de validacao do formulario de pagamento com zod

---
+++ src/utils/schemaPagamento.js
@@ -0,0 +1,23 @@
+import { validate, z } from "zod"
+
+export const schemaPagamento = z.object ({
+    titular: z
+        .string()
+        .trim()
+        .min(1, "Informe o nome do titular"),
+
+    numeroCartao: z
+        .string()
+        .transform((valor) => valor.replace(/\D/g, ""))
+        .refine((valor) => valor.length === 16, {
+            message: "O cartão deve ter 16 digitos",
+        }),
+    
+    validate: z
+        .string()
+        .regex(/^(0[1-9]|1[0-2])\/\d{2}$/, "Formato inválido, use MM/AA"),
+
+    cvv: z
+        .string()
+        .regex(/^\d{3}$/, "CVV deve ter 3 dígitos" )
+})
