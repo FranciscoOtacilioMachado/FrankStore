4401944d74be72692075b39b59c7c3643f08dbec
franciscootacilio <85141115+fransciscootacilio@users.noreply.github.com>
2026-09-13 09:14:51 -0300

    correcao da mensagem de erro para o solicitado no projeto

--- src/pages/ErroPagamento.jsx
+++ src/pages/ErroPagamento.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 function ErroPagamento() {
   return (
     <main className="pagina-resultado erroPagamento" >
-      <h1>Erro no pagamento</h1>     
+      <h1>Tentativa de golpe</h1>     
       <Link to="/pagamento">Tentar novamente</Link>
     </main>
   )
