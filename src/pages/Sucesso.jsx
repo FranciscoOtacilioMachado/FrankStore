bf958e6500e6d2fde2c856a70b80df1e6a0b4a06
franciscootacilio <85141115+fransciscootacilio@users.noreply.github.com>
2026-09-13 07:56:12 -0300

    estilizacao das paginas de sucesso e erro

--- src/pages/Sucesso.jsx
+++ src/pages/Sucesso.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 function Sucesso() {
     return (
-        <main>
+        <main className="pagina-resultado sucesso">
             <h1>Compra aprovada com sucesso!</h1>
             <Link to="/">Voltar ao carrinho</Link>
         </main>
